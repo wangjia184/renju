1804323bcfd901b3acf62038a697e498d5039559
jerry.wang <jerry.wang@everymatrix.com>
2022-09-18 09:48:33 +0800

    added about

--- game/ui/src/App.svelte
+++ game/ui/src/App.svelte
@@ -4,7 +4,13 @@
   import whiteBasketImage from "./assets/white_basket.png";
   import lostImage from "./assets/lost.gif";
   import wonImage from "./assets/won.gif";
-  import { Modal, ModalBody, ModalHeader, Offcanvas } from "sveltestrap";
+  import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalHeader,
+    Offcanvas,
+  } from "sveltestrap";
   import { invoke } from "@tauri-apps/api";
 
   let isColorSelectionOpen = true;
@@ -13,6 +19,7 @@
   let humanPlayBlack = true;
   let blocking = false;
   let clientHeight = 10;
+  let isAboutOpen = false;
   const startNewGame = (black) => {
     if (blocking) {
       return;
@@ -37,7 +44,11 @@
 </script>
 
 <main class="d-flex flex-row gap-2 h-100">
-  <div class="justify-content-center align-self-center h-100" bind:clientHeight={clientHeight} style="width: {clientHeight}px">
+  <div
+    class="justify-content-center align-self-center h-100"
+    bind:clientHeight
+    style="width: {clientHeight}px"
+  >
     <Board on:over={onGameOver} />
   </div>
   <div class="d-flex flex-column flex-grow-1">
@@ -47,7 +58,22 @@
         ? whiteBasketImage
         : blackBasketImage})"
     />
-    <div class="flex-grow-1" />
+    <div
+      class="flex-grow-1 justify-content-center align-self-center d-flex align-items-center gap-1"
+    >
+      <Button
+        color="secondary"
+        outline
+        size="sm"
+        on:click={() => (isColorSelectionOpen = true)}>Restart</Button
+      >
+      <Button
+        color="secondary"
+        outline
+        size="sm"
+        on:click={() => (isAboutOpen = true)}>About</Button
+      >
+    </div>
     <div
       class="bottom_basket"
       style="background-image: url({humanPlayBlack
@@ -78,6 +104,37 @@
         />
       </div>
     </div>
+  </ModalBody>
+</Modal>
+
+<Modal
+  isOpen={isAboutOpen}
+  class="modal-dialog-centered "
+  toggle={() => (isAboutOpen = !isAboutOpen)}
+>
+  <ModalHeader>&#20851;&#20110;</ModalHeader>
+  <ModalBody>
+    <ul>
+      <li>
+        &#22522;&#20110;&#38376;&#29305;&#21345;&#27931;&#25628;&#32034;&#26641;&#19982;&#21367;&#31215;&#31070;&#32463;&#32593;&#32476;&#30340;&#24102;&#31105;&#25163;&#20116;&#23376;&#26827;&#28216;&#25103;&#65292;&#23454;&#36341;&#24378;&#21270;&#23398;&#20064;&#21450;&#20854;&#29702;&#35770;&#12290;
+      </li>
+      <li>
+        &#20316;&#32773;&#65306;&#26480;&#29790;&#29579;
+        https://github.com/wangjia184/renju
+      </li>
+      <li>&#21442;&#32771;&#36164;&#26009;&#65306;</li>
+      <li>
+        <ol>
+          <li>https://joshvarty.github.io/AlphaZero/</li>
+          <li>
+            https://jonathan-hui.medium.com/alphago-zero-a-game-changer-14ef6e45eba5
+          </li>
+          <li>
+            https://jonathan-hui.medium.com/alphago-how-it-works-technically-26ddcc085319
+          </li>
+        </ol>
+      </li>
+    </ul>
   </ModalBody>
 </Modal>
 
