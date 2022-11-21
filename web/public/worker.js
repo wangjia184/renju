d89a7f447be3b407065bc2803659efb40642e89a
jerry.wang <jerry.wang@everymatrix.com>
2022-11-21 08:40:17 +0800

    using WASM backend of tensorflow

--- web/public/worker.js
+++ web/public/worker.js
@@ -1,59 +1,73 @@
-self.postMessage({ type : 'progress', progress : 0, message : 'Loading tensorflow'});
+self.postMessage({ type: 'progress', progress: 0, message: 'Loading tensorflow' });
 
 import("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.1.0/dist/tf.min.js").then(async () => {
 
     // successfully loaded tensorflow.js
-    self.postMessage({ type : 'progress', progress : 0.3, message : 'Loading model'});
+    self.postMessage({ type: 'progress', progress: 0.1, message: 'Loading tensorflow WASM backend' });
+
+    try {
+        let backend = await import('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@4.1.0/dist/tf-backend-wasm.min.js');
+        self.postMessage({ type: 'progress', progress: 0.2, message: 'Initialize tensorflow WASM backend' });
+
+        tf.wasm.setWasmPaths('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@4.1.0/dist/');
+
+        await tf.setBackend('wasm');
+        self.postMessage({ type: 'progress', progress: 0.3, message: 'Loading model' });
+    } catch (e) {
+        self.postMessage({ type: 'error', message: 'Failed to load tensorflow WASM backend' });
+        console.log('Failed to initialize tensorflow WASM backend', e);
+        return null;
+    }
 
     const model = await (async () => {
         try {
             return await tf.loadGraphModel('./ai/model.json');
         }
-        catch(e) {
+        catch (e) {
             console.log('Failed to load model', e);
             return null;
         }
     })();
 
-    if( !model ) {
-        self.postMessage({ type : 'error', message : 'Failed to load model'});
+    if (!model) {
+        self.postMessage({ type: 'error', message: 'Failed to load model' });
         return;
     } else {
-        self.postMessage({ type : 'progress', progress : 0.6, message : 'Loading WASM module'});
+        self.postMessage({ type: 'progress', progress: 0.6, message: 'Loading WASM module' });
     }
 
-    
+
 
 
     const wasm = await (async () => {
         try {
             return await import("./ai/renjuai.js");
         }
-        catch(e) {
+        catch (e) {
             console.log('Failed to load renjuai.js', e);
             return null;
         }
     })();
 
-    if( !model ) {
-        self.postMessage({ type : 'error', message : 'Failed to load WASM module'});
+    if (!model) {
+        self.postMessage({ type: 'error', message: 'Failed to load WASM module' });
         return;
     } else {
-        self.postMessage({ type : 'progress', progress : 0.9, message : 'Initializing WASM module'});
+        self.postMessage({ type: 'progress', progress: 0.9, message: 'Initializing WASM module' });
     }
-    
-    
+
+
     try {
         await wasm.default();
-        self.postMessage({ type : 'ready' });
+        self.postMessage({ type: 'ready' });
     }
-    catch(e) {
+    catch (e) {
         console.log('Failed to initialize WASM module', e);
-        self.postMessage({ type : 'error', message : 'Failed to initialize WASM module'});
+        self.postMessage({ type: 'error', message: 'Failed to initialize WASM module' });
         return;
     }
 
-    
+
 
     self.predict = (state_tensor) => {
         const input = tf.tensor([state_tensor]);
@@ -72,12 +86,14 @@ import("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.1.0/dist/tf.min.js").the
         return prediction;
     };
 
+    wasm.test('s');
+
 
 })
-.catch((err) => {
-    self.postMessage({ type : 'error', message : 'Unable to load tensorflow.js'});
-    console.log('Unable to load tensorflow.js', err);
-});
+    .catch((err) => {
+        self.postMessage({ type: 'error', message: 'Unable to load tensorflow.js' });
+        console.log('Unable to load tensorflow.js', err);
+    });
 
 
 
